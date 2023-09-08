caaaf8f6fcafcab1265618ad02657f5ead8cfda8
renhiyama <renhiyama@rovelstars.com>
2023-09-08 15:44:37 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div>
-      <h1 className="text-4xl text-center text-indigo-500 font-black">React App</h1>
+      <h1 className="text-4xl text-center text-indigo-500 font-black">Vite On Deno</h1>
     </div>
   )
 }
